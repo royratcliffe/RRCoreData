ede15ae266d39747ed0db6a93284a64b06f444ff
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2011-10-10 11:36:15 +0100

    Initial commit

---
+++ RRCoreDataTests/RRCoreDataTests.h
@@ -0,0 +1,13 @@
+//
+//  RRCoreDataTests.h
+//  RRCoreDataTests
+//
+//  Created by Roy Ratcliffe on 10/10/2011.
+//  Copyright (c) 2011 Pioneering Software, United Kingdom. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface RRCoreDataTests : SenTestCase
+
+@end
---
+++ RRCoreDataTests/RRCoreDataTests.m
@@ -0,0 +1,32 @@
+//
+//  RRCoreDataTests.m
+//  RRCoreDataTests
+//
+//  Created by Roy Ratcliffe on 10/10/2011.
+//  Copyright (c) 2011 Pioneering Software, United Kingdom. All rights reserved.
+//
+
+#import "RRCoreDataTests.h"
+
+@implementation RRCoreDataTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in RRCoreDataTests");
+}
+
+@end
